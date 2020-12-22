4b834788d91136c2e66c7d3d283d707c78bab2d2
Hung Jin Chong <hungjinchong@Outlook.com>
2020-12-22 13:27:54 +0000

    dsfa

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -182,140 +182,146 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        {!this.state.currentQuestion.answers ? (
-          <>
-            {this.state.username === "admin" ? (
-              <Form onSubmit={this.handleSubmit}>
-                <Form.Group>
-                  <Form.Label>Number of Questions</Form.Label>
-                  <Form.Control
-                    type="number"
-                    defaultValue="10"
-                    id="noOfQues"
-                    onChange={this.inputOnChange}
-                  />
-                  <Form.Label>Select Category</Form.Label>
-                  <Form.Control
-                    as="select"
-                    onChange={this.inputOnChange}
-                    id="category"
-                  >
-                    <option value="9">General Knowledge</option>
-                    <option value="10">Entertainment: Books</option>
-                    <option value="11">Entertainment: Film</option>
-                    <option value="12">Entertainment: Music</option>
-                    <option value="13">
-                      Entertainment: Musicals &amp; Theatres
-                    </option>
-                    <option value="14">Entertainment: Television</option>
-                    <option value="15">Entertainment: Video Games</option>
-                    <option value="16">Entertainment: Board Games</option>
-                    <option value="17">Science &amp; Nature</option>
-                    <option value="18">Science: Computers</option>
-                    <option value="19">Science: Mathematics</option>
-                    <option value="20">Mythology</option>
-                    <option value="21">Sports</option>
-                    <option value="22">Geography</option>
-                    <option value="23">History</option>
-                    <option value="24">Politics</option>
-                    <option value="25">Art</option>
-                    <option value="26">Celebrities</option>
-                    <option value="27">Animals</option>
-                    <option value="28">Vehicles</option>
-                    <option value="29">Entertainment: Comics</option>
-                    <option value="30">Science: Gadgets</option>
-                    <option value="31">
-                      Entertainment: Japanese Anime &amp; Manga
-                    </option>
-                    <option value="32">
-                      Entertainment: Cartoon &amp; Animations
-                    </option>
-                  </Form.Control>
-                  <Form.Label>Select Difficulty</Form.Label>
-                  <Form.Control
-                    as="select"
-                    onChange={this.inputOnChange}
-                    id="difficulty"
-                  >
-                    <option value="easy">Easy</option>
-                    <option value="medium">Medium</option>
-                    <option value="hard">Hard</option>
-                  </Form.Control>
-                </Form.Group>
-                <Button type="submit">Get Questions</Button>
-                {this.state.loading ? (
-                  <Spinner animation="border" role="status">
-                    <span className="sr-only">Loading...</span>
-                  </Spinner>
-                ) : (
-                  <Button className="ml-3" onClick={this.handleStart}>
-                    Start Quiz
-                  </Button>
-                )}
-              </Form>
-            ) : (
-              <h1>Wait for admin to start quiz</h1>
-            )}
-          </>
+      <>
+        {this.state.counter + 1 === this.state.questions.length ? (
+          <h2>Quiz Completed</h2>
         ) : (
-          <>
-            <h2>TRIVIA</h2>
-            <Card>
-              <Card.Header as="h5">
-                {this.state.currentQuestion.question.replace(
-                  /&#?\w+;/g,
-                  (match) => entities[match]
-                )}
-              </Card.Header>
-              <Card.Body>
-                <Card.Text>
-                  <ListGroup style={{ display: this.state.answered }}>
-                    {this.state.currentQuestion.answers.map((answer, index) => (
-                      <ListGroup.Item
-                        key={index}
-                        id={answer}
-                        onClick={this.handleSubmitAnswer}
+          <div>
+            {!this.state.currentQuestion.answers ? (
+              <>
+                {this.state.username === "admin" ? (
+                  <Form onSubmit={this.handleSubmit}>
+                    <Form.Group>
+                      <Form.Label>Number of Questions</Form.Label>
+                      <Form.Control
+                        type="number"
+                        defaultValue="10"
+                        id="noOfQues"
+                        onChange={this.inputOnChange}
+                      />
+                      <Form.Label>Select Category</Form.Label>
+                      <Form.Control
+                        as="select"
+                        onChange={this.inputOnChange}
+                        id="category"
                       >
-                        {index
-                          .toString()
-                          .replace(/^[0-9]+$/g, (match) => options[match])}
-                        .{" "}
-                        {answer.replace(/&#?\w+;/g, (match) => entities[match])}
-                      </ListGroup.Item>
-                    ))}
-                  </ListGroup>
-                </Card.Text>
-                {this.state.username === "admin" && (
-                  <>
-                    <Button variant="primary" onClick={this.handleNext}>
-                      Next
-                    </Button>
-                  </>
+                        <option value="9">General Knowledge</option>
+                        <option value="10">Entertainment: Books</option>
+                        <option value="11">Entertainment: Film</option>
+                        <option value="12">Entertainment: Music</option>
+                        <option value="13">
+                          Entertainment: Musicals &amp; Theatres
+                        </option>
+                        <option value="14">Entertainment: Television</option>
+                        <option value="15">Entertainment: Video Games</option>
+                        <option value="16">Entertainment: Board Games</option>
+                        <option value="17">Science &amp; Nature</option>
+                        <option value="18">Science: Computers</option>
+                        <option value="19">Science: Mathematics</option>
+                        <option value="20">Mythology</option>
+                        <option value="21">Sports</option>
+                        <option value="22">Geography</option>
+                        <option value="23">History</option>
+                        <option value="24">Politics</option>
+                        <option value="25">Art</option>
+                        <option value="26">Celebrities</option>
+                        <option value="27">Animals</option>
+                        <option value="28">Vehicles</option>
+                        <option value="29">Entertainment: Comics</option>
+                        <option value="30">Science: Gadgets</option>
+                        <option value="31">
+                          Entertainment: Japanese Anime &amp; Manga
+                        </option>
+                        <option value="32">
+                          Entertainment: Cartoon &amp; Animations
+                        </option>
+                      </Form.Control>
+                      <Form.Label>Select Difficulty</Form.Label>
+                      <Form.Control
+                        as="select"
+                        onChange={this.inputOnChange}
+                        id="difficulty"
+                      >
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                      </Form.Control>
+                    </Form.Group>
+                    <Button type="submit">Get Questions</Button>
+                    {this.state.loading ? (
+                      <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                      </Spinner>
+                    ) : (
+                      <Button className="ml-3" onClick={this.handleStart}>
+                        Start Quiz
+                      </Button>
+                    )}
+                  </Form>
+                ) : (
+                  <h1>Wait for admin to start quiz</h1>
                 )}
-              </Card.Body>
-            </Card>
-            <ListGroup style={{ display: this.state.scores }}>
-              <ListGroup.Item>
-                Correct Answer: {this.state.currentQuestion.correct_answer}
-              </ListGroup.Item>
-              {this.state.users.map((user) => (
-                <ListGroup.Item>
-                  {user.user} answered {user.answer}: {user.score}
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </>
+              </>
+            ) : (
+              <>
+                <h2>TRIVIA</h2>
+                <Card>
+                  <Card.Header as="h5">
+                    {this.state.currentQuestion.question.replace(
+                      /&#?\w+;/g,
+                      (match) => entities[match]
+                    )}
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Text>
+                      <ListGroup style={{ display: this.state.answered }}>
+                        {this.state.currentQuestion.answers.map(
+                          (answer, index) => (
+                            <ListGroup.Item
+                              key={index}
+                              id={answer}
+                              onClick={this.handleSubmitAnswer}
+                            >
+                              {index
+                                .toString()
+                                .replace(
+                                  /^[0-9]+$/g,
+                                  (match) => options[match]
+                                )}
+                              .{" "}
+                              {answer.replace(
+                                /&#?\w+;/g,
+                                (match) => entities[match]
+                              )}
+                            </ListGroup.Item>
+                          )
+                        )}
+                      </ListGroup>
+                    </Card.Text>
+                    {this.state.username === "admin" && (
+                      <>
+                        <Button variant="primary" onClick={this.handleNext}>
+                          Next
+                        </Button>
+                      </>
+                    )}
+                  </Card.Body>
+                </Card>
+                <ListGroup style={{ display: this.state.scores }}>
+                  <ListGroup.Item>
+                    Correct Answer: {this.state.currentQuestion.correct_answer}
+                  </ListGroup.Item>
+                  {this.state.users.map((user) => (
+                    <ListGroup.Item>
+                      {user.user} answered {user.answer}: {user.score}
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </>
+            )}
+          </div>
         )}
-
-        {/* <Accordion className="mt-3">
-          {this.state.questions.map((question, index) => {
-            return (
-              <Cards question={question} key={index} eventKey={index + 1} />
-            );
-          })}
-        </Accordion> */}
-      </div>
+      </>
     );
   }
 }
